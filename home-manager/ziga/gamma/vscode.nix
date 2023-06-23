c7e8f697ee0edd7cb38700e0142800d758d90b85
siggsy <ziga@leskovec.xyz>
2023-06-23 16:05:56 +0200

    Added missing vscode.nix

---
+++ home-manager/ziga/gamma/vscode.nix
@@ -0,0 +1,33 @@
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  programs.vscode = {
+    enable = true;
+    enableExtensionUpdateCheck = false;
+    mutableExtensionsDir = false;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      # Vscode
+      vscodevim.vim
+      pkief.material-icon-theme
+
+      # Nix
+      bbenoist.nix
+
+      # Java
+      redhat.java
+      vscjava.vscode-maven
+      vscjava.vscode-gradle
+      vscjava.vscode-java-debug
+      vscjava.vscode-java-dependency
+      vscjava.vscode-java-test
+    ];
+    userSettings = {
+      # Visibility
+      "window.menuBarVisibility" = "toggle";
+      "workbench.iconTheme" = "material-icon-theme";
+
+      # Editor
+      "editor.fontFamily" = "Victor Mono";
+      "editor.fontWeight" = "600";
+    };
+  };
+}
